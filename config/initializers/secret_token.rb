0355538aabf18e15009f66e58951212f2d2529c1
Paul Sutcliffe <paul@kosmyka.com>
2013-06-18 10:18:16 -0500

    Commit Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rio::Application.config.secret_token = '4fe4aba00ca0c8971c9002a562beb6ceadf56e05fec5edeeb9b78c7836fa8ab855f697a26c04c5b6227792d9be0a241f48145113f1ce4d3680297a1001598774'
