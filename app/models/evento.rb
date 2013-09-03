87c49d869d8d8d383d146180bf411d581ab4545c
Paul Sutcliffe <paul@kosmyka.com>
2013-09-03 01:13:56 -0500

    orden de la lista de eventos del home

--- app/models/evento.rb
+++ app/models/evento.rb
@@ -1,6 +1,6 @@
 class Evento < ActiveRecord::Base
   attr_accessible :fecha, :lugar, :nombre
 
-  scope :proximos, lambda { where("fecha >= ?", Time.now).order("fecha DESC") }
+  scope :proximos, lambda { where("fecha >= ?", Time.now).order("fecha ASC") }
   scope :pasados, lambda { where("fecha < ?", Time.now).order("fecha DESC") }
 end
